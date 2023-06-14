01f42c45d042e0cf2eccc611b70c66f84a74ca4d
CaelSerrao <86877852+CaelSerrao@users.noreply.github.com>
2023-06-14 09:38:52 +0400

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -95,12 +95,7 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
+
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     question = models.CharField(max_length=200, default="question")
@@ -121,12 +116,7 @@ class Question(models.Model):
             return False
 
 
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
+
 class Choice(models.Model):
 
     questions = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -136,10 +126,7 @@ class Choice(models.Model):
         return "question:{self.questions.question} choices:{self.choices} answer:{self.is_correct}"
 
 
-# <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
+
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
